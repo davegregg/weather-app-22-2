531a2d4e3f93e6ca0d806fce1befdb77d7176bd0
Jon Taylor <jontaylor224@gmail.com>
2022-07-21 12:07:57 -0500

    added rendering

--- view.js
+++ view.js
@@ -1,7 +1,28 @@
+let mainElement = document.querySelector('main')
+
 function renderHourlyForecasts (forecasts) {
     // render to DOM
     console.log(forecasts)
-    document.body.append(JSON.stringify(forecasts))
+
+    forecasts.forEach(renderForecast)
+    
+}
+
+function renderForecast (forecastObj) {
+    let forecastCard = document.createElement("section")
+        forecastCard.classList.add("card")
+    
+        let iconElement = document.createElement("img")
+        iconElement.src = forecastObj.icon
+        
+        let temperatureElement = document.createElement("p")
+        temperatureElement.innerText = `${forecastObj.temperature}° ${forecastObj.temperatureUnit}`
+        
+        let conditionsElement = document.createElement("p")
+        conditionsElement.innerText = forecastObj.shortForecast
+    
+        forecastCard.append(iconElement, temperatureElement, conditionsElement)
+        mainElement.append(forecastCard)
 }
 
 export { renderHourlyForecasts }
