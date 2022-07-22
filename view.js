4f336a65320b75597d007e047def523391d19d6f
Jon Taylor <jontaylor224@gmail.com>
2022-07-22 09:36:06 -0500

    fixed indentation

--- view.js
+++ view.js
@@ -1,28 +1,28 @@
 let mainElement = document.querySelector('main')
 
-function renderHourlyForecasts (forecasts) {
+function renderHourlyForecasts(forecasts) {
     // render to DOM
     console.log(forecasts)
 
     forecasts.forEach(renderForecast)
-    
+
 }
 
-function renderForecast (forecastObj) {
+function renderForecast(forecastObj) {
     let forecastCard = document.createElement("section")
-        forecastCard.classList.add("card")
-    
-        let iconElement = document.createElement("img")
-        iconElement.src = forecastObj.icon
-        
-        let temperatureElement = document.createElement("p")
-        temperatureElement.innerText = `${forecastObj.temperature}° ${forecastObj.temperatureUnit}`
-        
-        let conditionsElement = document.createElement("p")
-        conditionsElement.innerText = forecastObj.shortForecast
-    
-        forecastCard.append(iconElement, temperatureElement, conditionsElement)
-        mainElement.append(forecastCard)
+    forecastCard.classList.add("card")
+
+    let iconElement = document.createElement("img")
+    iconElement.src = forecastObj.icon
+
+    let temperatureElement = document.createElement("p")
+    temperatureElement.innerText = `${forecastObj.temperature}° ${forecastObj.temperatureUnit}`
+
+    let conditionsElement = document.createElement("p")
+    conditionsElement.innerText = forecastObj.shortForecast
+
+    forecastCard.append(iconElement, temperatureElement, conditionsElement)
+    mainElement.append(forecastCard)
 }
 
 export { renderHourlyForecasts }
